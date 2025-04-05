f55ebc9b7e68445a13f3c030ff6c801b66b255d9
naolumer <naolumer3@gmail.com>
2025-04-05 22:27:36 +0300

    tailwind config / added color primary

--- frontend/polling-app/tailwind.config.js
+++ frontend/polling-app/tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#06b6d4",
+        secondary: "#EF863E",
+      },
+
+      backgroundImage: {
+        'auth-bg-img': "url('./src/assets/images/auth-img.png')",
+        'profile-bg--img': "url('./src/assets/images/profile-bg.png')",
+      }
+    },
   },
   plugins: [],
 }
