5b74d8ccd5194dac6690d87898ccffe873382fdd
naolumer <naolumer3@gmail.com>
2025-05-24 21:49:24 +0300

    update

--- frontend/src/components/input/AuthInput.jsx
+++ frontend/src/components/input/AuthInput.jsx
@@ -1,9 +1,8 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { FaRegEye, FaRegEyeSlash } from "react-icons/fa6";
 
 const AuthInput = ({ value, onChange, label, placeholder, type }) => {
   const [showPassword, setShowPassword] = useState(false);
-
   const toggleShowPassword = () => {
     setShowPassword(!showPassword);
   };
